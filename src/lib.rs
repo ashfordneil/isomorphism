4bcfe5e9f6914e1c7b813b170bdaf8188e9f1f29
Neil Ashford <ashfordneil0@gmail.com>
2018-04-22 23:49:06 +1000

    fixed the bug where shuffled values didn't have their ideal neighbourhoods updated

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,6 @@ const RESIZE_GROWTH_FACTOR: usize = 2;
 // left as a fraction to avoid floating point multiplication and division where it isn't needed
 pub(crate) const MAX_LOAD_FACTOR: f32 = 1.1;
 
-
 /// The two way hashmap itself. See the module level documentation for more information.
 ///
 /// L and R are the left and right types being mapped to eachother. LH and RH are the hash builders
@@ -142,66 +141,95 @@ where
         data[ideal_index].neighbourhood = data[ideal_index].neighbourhood | B::one_at(offset);
     }
 
-    /// Finds (or makes) a free space in which to insert a key in the hashmap. If this is not
-    /// possible, due to the hashmap being too full, then None is returned.
-    fn find_insert_index<K, V>(
-        ideal_index: usize,
+    /// Finds the bitflield associated with an ideal hash index in a hashmap array, and mark a
+    /// given index as empty.
+    fn mark_as_empty<K>(ideal_index: usize, actual_index: usize, data: &mut [Bucket<K, usize, B>]) {
+        let offset = (data.len() + actual_index - ideal_index) % data.len();
+        data[ideal_index].neighbourhood = data[ideal_index].neighbourhood & B::zero_at(offset);
+    }
+
+    /// Inserts a given key into its data bucket. As this may do reshuffling, it requires a
+    /// reference to the value data buckets also. Returns, if it was possible to insert the value,
+    /// the index to which it was inserted. If it was not possible to do the insert, returns the
+    /// key that was going to be inserted. If this function returns successfully, it is guaranteed
+    /// that the key is located at the index specified, but its matching value is not set to
+    /// anything meaningful. This is the callers responsibility.
+    fn insert_one_sided<K: Hash, V, H: BuildHasher>(
+        key: K,
         key_data: &mut [Bucket<K, usize, B>],
         value_data: &mut [Bucket<V, usize, B>],
-    ) -> Option<usize> {
+        hasher: &H
+    ) -> Result<usize, K> {
         let len = key_data.len();
+        let ideal_index = Self::find_ideal_index(&key, hasher, len);
 
-        // check that the neighbourhood isn't full - if it is the hashmap needs to be resized
         if key_data[ideal_index].neighbourhood.full() {
-            return None;
+            return Err(key);
         }
 
-        // find the nearest free space
-        if let Some((offset, _)) = key_data[ideal_index..]
+        let nearest = key_data[ideal_index..]
             .iter()
             .chain(key_data[..ideal_index].iter())
             .enumerate()
             .find(|&(_, bucket)| bucket.data.is_none())
-        {
+            .map(|(offset, _)| offset);
+        if let Some(offset) = nearest {
             // is this free space within the neighbourhood?
             if offset < B::size() {
-                Some((offset + ideal_index) % len)
+                // insert and we're done
+                let index = (offset + ideal_index) % len;
+                Self::mark_as_full(ideal_index, index, key_data);
+                key_data[index].data = Some((key, usize::max_value(), ideal_index));
+                Ok(index)
             } else {
-                // reshuffle the hashmap to make room for the next element
-                let mut offset_index = (ideal_index + offset) % len;
-                loop {
-                    // find an element that can be shuffled into the free space
-                    match (0..)
-                        .map(|i| (len + offset_index - i) % len)
-                        .take(B::size())
-                        .skip(1)
-                        .find(|&i| {
-                            let &(_, _, ideal) = key_data[i].data.as_ref().unwrap();
-                            ideal > ideal_index && ideal < offset_index
-                        }) {
-                        Some(index) => {
-                            // move the found element into the free space
-                            let bucket = key_data[index].data.take().unwrap();
-                            value_data[bucket.1].data.as_mut().unwrap().1 = offset_index;
-                            key_data[offset_index].data = Some(bucket);
-                            offset_index = index;
-
-                            // either return the newly created free space, if it's close enough, or
-                            // repeat - finding another element to move into this free space
-                            if (offset_index + len - ideal_index) % len < B::size() {
-                                break Some(offset_index);
+                // need to make room -> find a space, boot the old thing out to make room, insert,
+                // repeat
+                let max_offset = (ideal_index + B::size()) % len;
+                let nearest = (0..)
+                    .map(|i| (len + max_offset - i) % len)
+                    .take(B::size())
+                    .skip(1)
+                    .find(|&i| {
+                        let &(_, _, ideal) = key_data[i].data.as_ref().unwrap();
+                        ideal > ideal_index || ideal < max_offset
+                    });
+                if let Some(index) = nearest {
+                    // we've found a spot to insert into
+                    let (new_key, new_value, new_ideal) = key_data[index].data.take().unwrap();
+                    key_data[index].data = Some((key, usize::max_value(), ideal_index));
+                    match Self::insert_one_sided(new_key, key_data, value_data, hasher) {
+                        Ok(new_key_index) => {
+                            // the replacement worked
+                            {
+                                let &mut (_, ref mut paired_key_index, _) =
+                                    value_data[new_value].data.as_mut().unwrap();
+                                *paired_key_index = new_key_index;
+                                let &mut (_, ref mut paired_value_index, _) =
+                                    key_data[new_key_index].data.as_mut().unwrap();
+                                *paired_value_index = new_value;
                             }
-                        }
-                        None => {
-                            break None;
+                            Self::mark_as_empty(new_ideal, index, key_data);
+                            Self::mark_as_full(new_ideal, new_key_index, key_data);
+
+                            // finish our insert and return
+                            Self::mark_as_full(ideal_index, index, key_data);
+                            Ok(index)
+                        },
+                        Err(new_key) => {
+                            // the replacement failed - undo our insert
+                            let (key, _, _) = key_data[index].data.take().unwrap();
+                            key_data[index].data = Some((new_key, new_value, new_ideal));
+                            Err(key)
                         }
                     }
+                } else {
+                    // no spot can be inserted into
+                    Err(key)
                 }
             }
         } else {
-            // the hashmap is entirely full - this should not be possible but if it happens just
-            // resize
-            None
+            // there is no free space
+            Err(key)
         }
     }
 
@@ -237,36 +265,40 @@ where
         };
 
         self.invariants();
-        let left_ideal_index =
-            Self::find_ideal_index(&left, &self.left_hasher, self.left_data.len());
-        let right_ideal_index =
-            Self::find_ideal_index(&right, &self.right_hasher, self.right_data.len());
 
-        let insert_indexes = {
-            let &mut BiMap {
-                ref mut left_data,
-                ref mut right_data,
-                ..
-            } = self;
-            let left_index = Self::find_insert_index(left_ideal_index, left_data, right_data);
-            let right_index = Self::find_insert_index(right_ideal_index, right_data, left_data);
+        // attempt to insert, hold onto the keys if it fails
+        let failure: Option<(L, R)> = {
+            let &mut BiMap { ref mut left_data, ref mut right_data, ref left_hasher, ref right_hasher } = self;
+            match Self::insert_one_sided(left, left_data, right_data, left_hasher) {
+                Ok(left_index) => {
+                    match Self::insert_one_sided(right, right_data, left_data, right_hasher) {
+                        Ok(right_index) => {
+                            let &mut (_, ref mut paired_right_index, _) =
+                                left_data[left_index].data.as_mut().unwrap();
+                            *paired_right_index = right_index;
 
-            if let (Some(left_index), Some(right_index)) = (left_index, right_index) {
-                Some((left_index, right_index))
-            } else {
-                None
+                            let &mut (_, ref mut paired_left_index, _) =
+                                right_data[right_index].data.as_mut().unwrap();
+                            *paired_left_index = left_index;
+                            None
+                        },
+                        Err(right) => {
+                            let (left, _, left_ideal) = left_data[left_index].data.take().unwrap();
+                            Self::mark_as_empty(left_ideal, left_index, left_data);
+                            Some((left, right))
+                        },
+                    }
+                },
+                Err(left) => {
+                    Some((left, right))
+                }
             }
         };
 
         self.invariants();
-        if let Some((left_index, right_index)) = insert_indexes {
-            Self::mark_as_full(left_ideal_index, left_index, &mut self.left_data);
-            Self::mark_as_full(right_ideal_index, right_index, &mut self.right_data);
 
-            self.left_data[left_index].data = Some((left, right_index, left_ideal_index));
-            self.right_data[right_index].data = Some((right, left_index, right_ideal_index));
-        } else {
-            /* resize */
+        if let Some((left, right)) = failure {
+            // resize, as we were unable to insert
             let capacity = self.left_data.len() * RESIZE_GROWTH_FACTOR;
             let old_left_data = mem::replace(&mut self.left_data, Bucket::empty_vec(capacity));
             let old_right_data = mem::replace(&mut self.right_data, Bucket::empty_vec(capacity));
